43042325fb01f8532bcb84490420e5b39248f7cd
Josselyn Mejia <josselynmejia572@gmail.com>
2025-06-17 10:07:51 -0400

    Created admin capabilities

--- NorthwindTradersSpringBoot/NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplication.java
+++ NorthwindTradersSpringBoot/NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplication.java
@@ -1,11 +1,19 @@
 package com.pluralsight.NorthwindTradersSpringBoot;
 
+import com.pluralsight.NorthwindTradersSpringBoot.dao.ProductDao;
+import com.pluralsight.NorthwindTradersSpringBoot.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class NorthwindTradersSpringBootApplication implements CommandLineRunner {
+
+	@Autowired
+	ProductDao productDao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(NorthwindTradersSpringBootApplication.class, args);
@@ -13,6 +21,9 @@ public class NorthwindTradersSpringBootApplication implements CommandLineRunner 
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		productDao.add(new Product(0, "Coke", "Drinks", 2));
+
+		List<Product> products = productDao.getAll();
+		System.out.println(products);
 	}
 }
---
+++ NorthwindTradersSpringBoot/NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/dao/ProductDao.java
@@ -0,0 +1,11 @@
+package com.pluralsight.NorthwindTradersSpringBoot.dao;
+
+import com.pluralsight.NorthwindTradersSpringBoot.model.Product;
+
+import java.util.List;
+
+public interface ProductDao {
+    List<Product> getAll();
+    Product add(Product product);
+
+}
