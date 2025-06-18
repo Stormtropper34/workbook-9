5a0f18544b56ff70167e6717b89fe168755f9f62
Josselyn Mejia <josselynmejia572@gmail.com>
2025-06-18 13:09:34 -0400

    initial commit

---
+++ demo.api/demo.api/src/main/java/com/pluralsight/demo/api/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.pluralsight.demo.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String index(@RequestParam(defaultValue = "Josselyn") String name){
+        return "Hello " + name;
+    }
+
+}
